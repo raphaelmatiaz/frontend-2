de35165922da049ecfdbcfbe49c08a05c9914ea0
Rafael Matias <rafamatias@gmail.com>
2025-02-18 20:36:39 +0000

    Aula 7

--- aula-4/vite/myReactApp/src/App.tsx
+++ aula-4/vite/myReactApp/src/App.tsx
@@ -1,41 +1,43 @@
 import './App.css'
-import Card from './components/Card/Card.tsx'
-import Greeting from './components/Greeting.tsx'
-import AccountBalance from './components/AccountBalance.tsx'
+// import Card from './components/Card/Card.tsx'
+// import Greeting from './components/Greeting.tsx'
+// import AccountBalance from './components/AccountBalance.tsx'
+import HomePage from './pages/Home.tsx'
+import AboutPage from './pages/About.tsx'
+import { Routes, Route } from 'react-router-dom'
 
-const cardA = {
-  title: "Very Fancy Card",
-  imgUrl: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTJpMTFtODhzNzJudmIyeXljNXdqc21wam45cHd4YXhiNmt1Z2duZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VbnUQpnihPSIgIXuZv/giphy.gif",
-  text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus dignissimos corporis, eum odit, ut maxime nobis amet fuga deleniti magni eveniet, quis ducimus praesentium. Accusamus dicta animi hic. Rerum debitis delectus maxime odit, ex molestiae commodi illo doloribus eaque.",
-  ctaText: "Click Me!",
-}
+// const cardA = {
+//   title: "Very Fancy Card",
+//   imgUrl: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExOTJpMTFtODhzNzJudmIyeXljNXdqc21wam45cHd4YXhiNmt1Z2duZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VbnUQpnihPSIgIXuZv/giphy.gif",
+//   text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur minus dignissimos corporis, eum odit, ut maxime nobis amet fuga deleniti magni eveniet, quis ducimus praesentium. Accusamus dicta animi hic. Rerum debitis delectus maxime odit, ex molestiae commodi illo doloribus eaque.",
+//   ctaText: "Click Me!",
+// }
 
-const cardB = {
-  title: "Cool Animated Card",
-  imgUrl:
-    "https://media2.giphy.com/media/l0HlMROGbUjFjv8GA/giphy.gif",
-  text: "This card is really cool and has animations! It's a fantastic addition to your collection. Enjoy the smooth motion and dynamic energy!",
-  ctaText: "Discover More",
-};
+// const cardB = {
+//   title: "Cool Animated Card",
+//   imgUrl:
+//     "https://media2.giphy.com/media/l0HlMROGbUjFjv8GA/giphy.gif",
+//   text: "This card is really cool and has animations! It's a fantastic addition to your collection. Enjoy the smooth motion and dynamic energy!",
+//   ctaText: "Discover More",
+// };
 
-const cardC = {
-  title: "Mysterious Card",
-  imgUrl:
-    "https://media3.giphy.com/media/26AHONQ79FdWZhAI0/giphy.gif",
-  text: "No one truly knows where this card came from. It appeared one day, carrying an air of mystery and intrigue. What secrets does it hold?",
-  ctaText: "Uncover the Secret",
-};
+// const cardC = {
+//   title: "Mysterious Card",
+//   imgUrl:
+//     "https://media3.giphy.com/media/26AHONQ79FdWZhAI0/giphy.gif",
+//   text: "No one truly knows where this card came from. It appeared one day, carrying an air of mystery and intrigue. What secrets does it hold?",
+//   ctaText: "Uncover the Secret",
+// };
 
 function App() {
 
   return (
-    <>
-      <AccountBalance balance={1000}></AccountBalance>
-      <Greeting name="Rafael"></Greeting>
-      <Card title={cardA.title} imgUrl={cardA.imgUrl} text={cardA.text} ctaText={cardA.ctaText}></Card>
-      <Card title={cardB.title} imgUrl={cardB.imgUrl} text={cardB.text} ctaText={cardB.ctaText}></Card>
-      <Card title={cardC.title} imgUrl={cardC.imgUrl} text={cardC.text} ctaText={cardC.ctaText}></Card>
-    </>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
+
   )
 }
 
